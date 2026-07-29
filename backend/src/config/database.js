56b04890eb25cbf24797c907a438d0f3926130cf
Gabriel Mugisha <gabrielmugisha08@gmail.com>
2026-07-29 23:04:54 +0200

    add ssl support for hosted postgres and netlify function deps

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -12,7 +12,7 @@ const pool = new Pool({
   database: process.env.DB_NAME     || "procurement_auditor",
   max:              10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
   ssl: isLocalHost ? false : { rejectUnauthorized: false },
 });
 
